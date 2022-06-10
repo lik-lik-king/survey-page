30ff2166f73c6410323747d2aafebaa773675710
Malik Muhammad <74846114+lik-lik-king@users.noreply.github.com>
2022-06-09 21:24:09 -0500

    Freecodecamp Survey Project
    Beginner survey form HTML project for Freecodecamp

---
+++ survey_page/lindsey-survey-form-page.js
@@ -0,0 +1,26 @@
+/*const charactersList = document.querySelector('.characters')
+console.log(charactersList)
+const humans = charactersList.querySelector('.humans')
+console.log(humans)
+const allHumans = humans.querySelectorAll('li')
+console.log(allHumans)
+const getHobbitsList = charactersList.querySelector('.hobbits')
+console.log(getHobbitsList)
+const allHobbits = getHobbitsList.children
+console.log(allHobbits)
+const merryHobbit = allHobbits[2]
+console.log(merryHobbit)
+const elves = merryHobbit.parentElement.nextElementSibling.nextElementSibling
+console.log(elves)
+const glorFindel = elves.children[1]
+console.log(glorFindel)
+const elrond = glorFindel.nextElementSibling
+console.log(elrond)
+const legolas = elrond.previousElementSibling.previousElementSibling
+console.log(legolas)
+const getDiv = legolas.parentElement.nextElementSibling
+console.log(getDiv)
+const nazgûl = document.querySelector('.enemies').children[1]
+console.log(nazgûl)
+const characters = nazgûl.closest('.characters')
+console.log(characters)*/
